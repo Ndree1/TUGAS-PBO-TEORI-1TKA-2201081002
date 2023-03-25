6d7bc485bc6b3f1a8237c291834fbf7ee2632c12
Ndree1 <afransisko672@gmail.com>
2023-03-25 10:52:16 +0700

    tugasmodul3

---
+++ TheThree/src/thethree/Modul3.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thethree;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class Modul3 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("I think that i shall never see,");
+        System.out.println("a poem as lovely as a tree.");
+         System.out.println("A tree whose hungry mouth is pressed");
+          System.out.println("Againts the Earth's sweet flowing breast.");
+   
+    }
+    
+}
